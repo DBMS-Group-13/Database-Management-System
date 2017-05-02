bd47af9c904bb6616c2a043c63485399a7dbfcfe
ZIhan Mu <mzh15301148@163.com>
2017-05-02 21:39:14 +0800

    Add files via upload

---
+++ Week1/main.cpp
@@ -0,0 +1,12 @@
+#include "loginWindow.h"
+#include"centerwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    loginWindow w;
+    w.show();
+
+    return a.exec();
+}
