02e2c9ad9222bd3f2665b3cab44eb4cf42da8291
Samuel You <15301164@bjtu.edu.cn>
2017-05-19 14:37:52 +0800

    Add files via upload

---
+++ week3/Util/appexception.cpp
@@ -0,0 +1,24 @@
+﻿#include "appexception.h"
+
+//	构造函数
+AppException::AppException(QString strError)
+{
+    m_strError = strError;
+    m_nCode = 0;
+}
+
+// 析构函数
+AppException::~AppException(void)
+{
+}
+
+/************************************************************************
+[函数名称]	GetErrorMessage
+[功能]	获取错误信息
+[参数]	void
+[返回值]	CString:异常信息
+************************************************************************/
+QString AppException::GetErrorMessage()
+{
+    return m_strError;
+}
